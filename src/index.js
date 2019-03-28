c84818a502b84b6bbadecfc41999f9c6b5e89d1b
Calvin Mann <calvinmann@gmail.com>
2019-03-28 20:26:30 +0100

    Work to get functioning version

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 module.exports = createGraphqlFirebaseAuthSource;
 
+const LIMIT = 200;
+
 const createGraphqlDatasource = require('@major-mann/graphql-datasource-base');
 createGraphqlFirebaseAuthSource.graphql = createGraphqlDatasource.graphql;
 
 async function createGraphqlFirebaseAuthSource({ auth, graphqlOptions }) {
     const definitions = createDefinitions();
-    const source = await createGraphqlDatasource({ data: loadCollection, definitions, graphqlOptions });
+    const source = await createGraphqlDatasource({
+        data: loadCollection,
+        definitions,
+        graphqlOptions,
+        rootTypes: ['User']
+    });
     return source;
 
     function createDefinitions() {
@@ -14,6 +21,22 @@ async function createGraphqlFirebaseAuthSource({ auth, graphqlOptions }) {
             type User {
                 uid: ID!
                 email: String
+                emailVerified: Boolean
+                phoneNumber: String
+                disabled: Boolean!
+            }
+
+            input UserInput {
+                email: String
+                password: String
+                emailVerified: Boolean
+                phoneNumber: String
+                disabled: Boolean!
+            }
+
+            input UserUpdateInput {
+                email: String
+                password: String
                 emailVerified: Boolean
                 phoneNumber: String
                 disabled: Boolean
@@ -71,13 +94,232 @@ async function createGraphqlFirebaseAuthSource({ auth, graphqlOptions }) {
             }
         }
 
-        async function list({ filter, order, cursor, limit }) {
-            // TODO: We should be able to do something here...
-            //  We could handle things like an email and phone number lookups
-            //  The per item cursor would have to specify the page and an offset
-            //  Would also be tricky to work with next and previous pages
-            throw new Error('firebase auth does not support listing users');
+        async function findByEmail(email) {
+            try {
+                const user = await auth.getUserByEmail(email);
+                return user;
+            } catch (ex) {
+                if (ex.code === 'auth/user-not-found') {
+                    return undefined;
+                } else {
+                    throw ex;
+                }
+            }
+        }
+
+        async function findByPhoneNumber(phoneNumber) {
+            try {
+                const user = await auth.getUserByPhoneNumber(phoneNumber);
+                return user;
+            } catch (ex) {
+                if (ex.code === 'auth/user-not-found') {
+                    return undefined;
+                } else {
+                    throw ex;
+                }
+            }
+        }
+
+        async function list({ filter, order, first, last, before, after }) {
+            if (first < 0) {
+                throw new Error('When supplied, first MUST be greater than or equal to 0');
+            }
+            if (last < 0) {
+                throw new Error('When supplied, last MUST be greater than or equal to 0');
+            }
+            if (before) {
+                throw new Error('firebase auth list does not support the before cursor');
+            }
+            if (first === 0 || last === 0) {
+                return empty();
+            }
+            if (first > 0 && last > 0 && first > last) {
+                // This just simplifies the conditions later
+                last = undefined;
+            }
+            if (first > 0 === false && last > 0 || last > first) {
+                throw new Error('firebase auth list does not support accessing data from the tail');
+            }
+            if (order && order.length) {
+                throw new Error('Auth service user listing does not support ordering');
+            }
+
+            const limit = calculateLimit();
+            if (limit > LIMIT) {
+                throw new Error('The maximum number of records that can be requested (using first and last) ' +
+                    `is ${LIMIT}. Received ${limit} (first: ${first}. last: ${last})`);
+            }
+
+            if (after) {
+                after = deserializeCursor(after);
+            }
+            if (after && after.field || filter && filter.length) {
+                return pseudoList(after);
+            } else {
+                return standardList(after);
+            }
+
+            async function standardList(cursor) {
+                let lim = limit;
+                if (cursor && cursor.offset > 0) {
+                    lim = lim + cursor.offset;
+                }
+                const listResult = await auth.listUsers(lim, cursor && cursor.list);
+                const users = [];
+                listResult.users.forEach(user => users.push(user));
+
+                // Remove any from the offset
+                if (cursor && cursor.offset > 0) {
+                    for (var userIndex = 0; userIndex < cursor.offset; userIndex++) {
+                        users[userIndex] = false;
+                    }
+                }
+                return buildResult(users);
+
+                function buildResult(users) {
+                    if (users) {
+                        return {
+                            edges: users
+                                .map(buildEdge)
+                                .filter(e => e),
+                            pageInfo: {
+                                hasPreviousPage: Boolean(cursor),
+                                hasNextPage: Boolean(listResult.pageToken)
+                            }
+                        };
+                    } else {
+                        return empty();
+                    }
+
+                    function buildEdge(user, index) {
+                        if (user) {
+                            const edgeCursor = index === users.length - 1 && listResult.pageToken ?
+                                serializeCursor({ list: listResult.pageToken, offset: 0 }) :
+                                serializeCursor({ list: cursor && cursor.list, offset: index + 1 });
+                            return {
+                                node: {
+                                    uid: user.uid,
+                                    email: user.email,
+                                    emailVerified: user.emailVerified,
+                                    phoneNumber: user.phoneNumber,
+                                    disabled: user.disabled
+                                },
+                                cursor: edgeCursor
+                            };
+                        } else {
+                            return undefined;
+                        }
+                    }
+                }
+            }
+
+            async function pseudoList(cursor) {
+                filter.forEach(validateFieldOperation);
+                let current, users;
+
+                if (cursor) {
+                    users = [await lookup(cursor.field, cursor.value)];
+                } else {
+                    current = filter.shift();
+                    users = [await lookup(current.field, current.value)];
+                }
+
+                // If we have additional, make sure all filters match the user
+                while (filter.length && users.length) {
+                    current = filter.shift();
+                    users = users.filter(user => user[current.field] === current.value);
+                }
+                return buildResult(users);
+
+                async function lookup(field, value) {
+                    switch (field) {
+                        case 'uid':
+                            user = await find(value);
+                            break;
+                        case 'email':
+                            user = await findByEmail(value);
+                            break;
+                        case 'phoneNumber':
+                            user = await findByPhoneNumber(value);
+                            break;
+                        default:
+                            throw new Error(`Filtering on "${field}" not supported`);
+                    }
+                    return user;
+                }
+
+                function buildResult(users) {
+                    if (users) {
+                        return {
+                            edges: users.map(buildEdge),
+                            pageInfo: {
+                                hasPreviousPage: false,
+                                hasNextPage: false
+                            }
+                        };
+                    } else {
+                        return empty();
+                    }
+
+                    function buildEdge(user) {
+                        // TODO: Can we avoid building the cursor based on requested fields?
+                        return {
+                            node: {
+                                uid: user.uid,
+                                email: user.email,
+                                emailVerified: user.emailVerified,
+                                phoneNumber: user.phoneNumber,
+                                disabled: user.disabled
+                            },
+                            cursor: serializeCursor({ field: 'uid', value: user.uid })
+                        };
+                    }
+                }
+
+                function validateFieldOperation(filter) {
+                    if (filter.op !== 'EQ') {
+                        throw new Error(`Filter operations on users only support comparison (equals) operations`);
+                    }
+                }
+            }
+
+            function empty() {
+                return {
+                    edges: [],
+                    pageInfo: {
+                        hasPreviousPage: false,
+                        hasNextPage: false
+                    }
+                };
+            }
+
+            function calculateLimit() {
+                if (first >=0 && last >= 0) {
+                    return Math.max(first, last);
+                } else if (first >= 0) {
+                    return first;
+                } else if (last >= 0) {
+                    return last;
+                } else {
+                    return LIMIT;
+                }
+            }
         }
     }
 
+    function serializeCursor({ field, value, list, offset }) {
+        const data = { f: field, v: value, l: list, o: offset };
+        return Buffer.from(JSON.stringify(data)).toString('base64');
+    }
+
+    function deserializeCursor(cursor) {
+        const buffer = Buffer.from(cursor, 'base64');
+        const data = JSON.parse(buffer.toString('utf8'));
+        return {
+            field: data.f,
+            value: data.v,
+            list: data.l,
+            offset: data.o
+        };
+    }
 }
